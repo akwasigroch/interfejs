d0b2135824cbc055ec29f47040e08fee6943fd2f
Arkadiusz Kwasigroch <arkadiusz.kwasigroch@pg.edu.pl>
2022-08-06 13:15:54 +0200

    next

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 from crypt import methods
-from flask import Flask, request
+from flask import Flask, request, redirect, flash
 from flask import render_template
+import os
 
 app = Flask(__name__)
+app.secret_key = "super secret key"
+app.config['UPLOAD_FOLDER'] = 'upload'
+
 
 @app.route('/form', methods=['POST'])
 def process_image():
-    if request.method =='POST':
-        print(request)
-        return 'ok'
+    if request.method == 'POST':
+        if 'image' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+
+        file = request.files['image']
+        if file.filename == '':
+            return "No selected file"
+        
+        if file:
+            filename = file.filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return "File loaded"
+
+
+
 
 @app.route("/")
 def home():
