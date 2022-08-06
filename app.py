d7fe32018eab651ad945e8ca00e4d54beb258155
Arkadiusz Kwasigroch <arkadiusz.kwasigroch@pg.edu.pl>
2022-08-06 17:18:49 +0200

    next

--- app.py
+++ app.py
@@ -2,31 +2,48 @@ from crypt import methods
 from flask import Flask, request, redirect, flash
 from flask import render_template
 import os
+from predict import classify
 
 app = Flask(__name__)
 app.secret_key = "super secret key"
-app.config['UPLOAD_FOLDER'] = 'upload'
+app.config['UPLOAD_FOLDER'] = 'static'
+
+
+
+@app.route('/form')
+def return_to_index():
+    return render_template('index.html')
 
 
 @app.route('/form', methods=['POST'])
 def process_image():
+    filename = False
+    probability = 5
     if request.method == 'POST':
         if 'image' not in request.files:
             flash('No file part')
-            return redirect(request.url)
+            state = 'no_file_part'
 
         file = request.files['image']
         if file.filename == '':
-            return "No selected file"
-        
+            state = 'no_selected_file'
+
         if file:
             filename = file.filename
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return "File loaded"
+            extension = filename.split('.')[-1]
+            filename = f'image.{extension}'
+            file_path =  os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            probability = int(classify(file_path) * 100)
+            state = 'ok'
 
-
+        return render_template('classification.html', state=state, filename=filename, probability=probability)
 
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+if __name__=='__main__':
+    app.run(host="0.0.0.0")
+
---
+++ predict.py
@@ -0,0 +1,52 @@
+from tkinter import W
+import torch
+import torch.nn as nn
+import torchvision
+from PIL import Image
+from torchvision.models.resnet import resnet50
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"]=""
+
+checkpoint_path = '/media/dysk/projects/self-supervised_learning/eksperymenty/do_paperu/stron_augmentation/rotation_imagenet_pretext_invariant/training4/_ckpt_epoch_63.ckpt' 
+
+transform = torchvision.transforms.Compose([
+                            torchvision.transforms.Resize((224,224)),
+                            torchvision.transforms.ToTensor(),
+                            torchvision.transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])])
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__() 
+
+
+        self.network = resnet50(pretrained = False)
+        self.network = torch.nn.Sequential(*list(self.network.children())[:-1])
+        
+        self.fc1 = nn.Linear(2048,1)
+
+    def forward(self,x ):
+        x = self.network(x)
+        x = x.view(-1,2048)
+        x = self.fc1(x)
+        x = torch.nn.Sigmoid()(x)
+        return x
+
+
+net = Net()
+state_dict = torch.load(checkpoint_path, map_location=torch.device('cpu'))['state_dict']
+net.load_state_dict(state_dict)
+net.eval()
+
+def classify(image_path):
+    image_path = image_path 
+    image = Image.open(image_path)
+    image = transform(image)
+
+    image = image[None,...]
+    result = net(image)
+    result = result.item()
+    return result
+
+
+
